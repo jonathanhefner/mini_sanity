d1cdcb31450de1e317a6cda6e65bddf2ef5d45d1
Jonathan Hefner <jonathan.hefner@gmail.com>
2017-08-24 13:01:48 -0500

    Add Object#assert_instance_of!

--- lib/mini_sanity/object.rb
+++ lib/mini_sanity/object.rb
@@ -20,4 +20,27 @@ class Object
     self
   end
 
+  # Checks that the Object is an instance of a given class, and returns
+  # the Object unmodified.  If the Object fails this check, an exception
+  # is raised.
+  #
+  # @example
+  #   "abc".assert_instance_of!(String)   # == "abc"
+  #   "abc".assert_instance_of!(Numeric)  # raises exception
+  #
+  # @param klass [Class]
+  #   class to check
+  # @param name [String, Symbol]
+  #   optional name to include in the error message
+  # @return [self]
+  # @raise [MiniSanity::Error]
+  #   if the Object is not an instance of +klass+
+  def assert_instance_of!(klass, name = nil)
+    unless self.instance_of?(klass)
+      prelude = name ? "#{name} is instance of #{self.class}" : "unexpected #{self.class}"
+      raise MiniSanity::Error.new("#{prelude}; expected #{klass}")
+    end
+    self
+  end
+
 end
--- test/mini_sanity/object_test.rb
+++ test/mini_sanity/object_test.rb
@@ -10,4 +10,12 @@ class ObjectTest < Minitest::Test
     refute_sanity {|name| nil.refute_nil!(name) }
   end
 
+  def test_assert_instance_of_pass
+    assert_sanity("abc") {|s| s.assert_instance_of!(String) }
+  end
+
+  def test_assert_instance_of_fail
+    refute_sanity {|name| "abc".assert_instance_of!(Numeric, name) }
+  end
+
 end
