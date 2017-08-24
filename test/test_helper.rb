92311c99b0f5d95c37ef154325fc2cc58f28c5e9
Jonathan Hefner <jonathan.hefner@gmail.com>
2017-08-24 12:24:35 -0500

    Add test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,17 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "mini_sanity"
 
 require "minitest/autorun"
+
+class Minitest::Test
+
+  def assert_sanity(subject, &block)
+    assert_equal subject, block.call(subject)
+  end
+
+  def refute_sanity(subject_name = "INSAAANE", &block)
+    assert_raises(MiniSanity::Error) { block.call(nil) }
+    error = assert_raises(MiniSanity::Error) { block.call(subject_name) }
+    assert_match subject_name, error.message
+  end
+
+end
